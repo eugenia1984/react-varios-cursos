19f1f168586b2fb24cf758366f540371f25db401
Eugenia <costamariaeugenia1@gmail.com>
2023-01-04 16:29:05 -0300

    creo App.js

---
+++ 05_react_hook_esencial/hooks_personalizados/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import useDocumentMousePosition from "./hooks/useDocumentMousePosition";
+
+function App() {
+  <div className="container>
+  </div>
+}
+
+export default App;
