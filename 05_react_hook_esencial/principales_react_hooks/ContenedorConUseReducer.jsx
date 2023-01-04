16eb0bb29b00ddfca497163144e9b4d75580e225
Eugenia <costamariaeugenia1@gmail.com>
2023-01-04 15:37:14 -0300

    actualizo ContenedorConUseReducer.jsx

--- 05_react_hook_esencial/principales_react_hooks/ContenedorConUseReducer.jsx
+++ 05_react_hook_esencial/principales_react_hooks/ContenedorConUseReducer.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useReducer } from "react";
+import MiComponente from "./MiComponente";
 
 export default function ContenedorConUseReducer() {
   const data = 
@@ -11,13 +12,32 @@ export default function ContenedorConUseReducer() {
       status: 0
     }
   
+  const reducer = ( estado, accion) => {
+     switch (accion.type) {
+       case "seleccionar":
+         return { ...estado, status: 1};
+       case "desSeleccionar":
+         return { ...estado, status: 0};
+       default:
+         throw new Error();
+     }
+  
+  }
+  
+  const [ estado, dispatch ] = useReducer( reducer, data );
+  
   return (
     <div className="container">
       <MiContexto.provider value={data} >
           <MiComponente />
       </MiContexto.Provider >
 
-<button className="mt-4">Cambiar estado</button>
+      <button 
+        className="mt-4"
+        onClick={ () => { dispatch({type:"seleccionar"})} }
+       >
+        Cambiar estado
+      </button>
     </div>
   )
 }
