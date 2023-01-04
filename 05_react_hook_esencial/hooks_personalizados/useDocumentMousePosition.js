e7b8b8436c7cae097b736bae14d723fa64b152a2
Eugenia <costamariaeugenia1@gmail.com>
2023-01-04 16:37:34 -0300

    actualizo useDocumentMousePosition.js

--- 05_react_hook_esencial/hooks_personalizados/useDocumentMousePosition.js
+++ 05_react_hook_esencial/hooks_personalizados/useDocumentMousePosition.js
@@ -4,9 +4,13 @@ const useDocumentMousePosition = () => {
   
   const [ isMouseOnRange, setIsMouseOnRange ] = useState(false);
   
+  const checkMousePosition = (event) => {
+    setIsMouseOnRange(event?.pageY < 150);
+  }
+  
   // se ejecuta en el momenot de inicializar el hook
   useEffect( () => {
-    
+    document.addEventListener("mouseover", checkMousePosition);
   }, [])
   
   return isMouseOnRange;
