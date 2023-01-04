0ae629a03200e6fab953d4e77e6247dc183717ca
Eugenia <costamariaeugenia1@gmail.com>
2023-01-04 14:47:15 -0300

    actualizo

--- 05_react_hook_esencial/principales_react_hooks/Contenedor.jsx
+++ 05_react_hook_esencial/principales_react_hooks/Contenedor.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import MiComponente from "/MiComponente";
 
 export default function Contenedor() {
   return (
     <div>
-    
+      <MiComponente />
     </div>
   )
 }
