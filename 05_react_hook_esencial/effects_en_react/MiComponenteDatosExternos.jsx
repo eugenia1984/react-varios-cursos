43f6fcf746ffe1aa355f66cc7e9b8f03e56afa4f
Eugenia <costamariaeugenia1@gmail.com>
2023-01-04 12:30:41 -0300

    agrego MiComponenteDatosExternos.jsx

---
+++ 05_react_hook_esencial/effects_en_react/MiComponenteDatosExternos.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+
+export default function MiComponenteDatosExternos() {
+
+  // vamos a importar los initial values de una fuente externa
+  const [ lista, setLista ] = useState( []);
+  
+  // para cuando se cambie la lista se usara este useEffect
+  useEffect( () => {
+    // actualizar la lista del carrito de compras
+    actualizarCarritoDeCompra();
+  }, [lista])
+  
+  const [ seleccionados, setSeleccionados ] = useState();
+  
+  const agregarALista = (e) => {
+    const nuevaLista = lista.map( (producto) => 
+      producto.nombre === e.target.value ? { ...producto, status: (e.target.checked)? 1 : 0} : producto
+    );
+
+    setLista(nuevaLista);
+  };
+  
+  const actualizarCarritoCompra = () => {
+    const nuevoCarrito = lista
+      .filter( (producto) => producto.status === 1)
+      .map( (producto) => producto.nombre);
+    
+    setSeleccionados(nuevoCarrito);
+    
+    
+  }
+  
+  const listaOpciones = lista.map( ({ nombre, precio }) => {
+    <li 
+      key={nombre.replace(" ", "").toLowerCase()} 
+      onChange={agregarLista}
+      className?"list-group-item"
+    > 
+        <input type="checkbox" value={nombre}/> ${precio} {nombre}
+    </li>
+  })
+  
+  return (
+    <div className="container">
+      <ul className="list-group> {listaOpciones} </ul>
+      <div className="alert alert-warning" role="alert">
+        {seleccionados.map( (tag, 1) => [1 < 0 && ", " , <>{tag}</>])}
+      </div>
+      <div></div>
+    </div>
+  );
+} 
