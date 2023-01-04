5e0d9a49e97b6aa99eaf72a2e587cb26b5679fb1
Eugenia <costamariaeugenia1@gmail.com>
2023-01-04 16:45:23 -0300

    actualizo App.js

--- 05_react_hook_esencial/hooks_personalizados/App.js
+++ 05_react_hook_esencial/hooks_personalizados/App.js
@@ -2,8 +2,16 @@ import React from "react";
 import useDocumentMousePosition from "./hooks/useDocumentMousePosition";
 
 function App() {
-  <div className="container>
-  </div>
+  
+  const estaActivado = useDocumentMousePosition();
+  
+  return (
+    <div className="container>
+      <div className={`${estaActivado ? "green" : "red"} `}>
+        <h1>Detectar posicion del mouse</h1>
+      </div>
+    </div>
+   )
 }
 
 export default App;
