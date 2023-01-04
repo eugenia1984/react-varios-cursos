1dfcb65283bb34094b41656e702e32113f8a7a61
Eugenia <costamariaeugenia1@gmail.com>
2023-01-04 14:38:47 -0300

    Create Contenedor.jsx

---
+++ 05_react_hook_esencial/principales_react_hooks/Contenedor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Contenedor() {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+// creo mi contexto
+export const MiContexto = React.createContext(null);
