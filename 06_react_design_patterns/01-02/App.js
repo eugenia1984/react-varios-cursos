c9dad594abba48734dbbc751dbd94db315efb766
Eugenia <costamariaeugenia1@gmail.com>
2023-01-06 10:36:49 -0300

    agrego ejemplo de layout patterns

---
+++ 06_react_design_patterns/01-02/App.js
@@ -0,0 +1,15 @@
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHandComponent = () => {
+  return <h1 style={{ backgroundColor: "green" }}>Left!</h1>;
+};
+
+const RightHandComponent = () => {
+  return <p style={{ backgroundColor: "red" }}>Right!</p>;
+};
+
+function App() {
+  return <SplitScreen left={LeftHandComponent} right={RightHandComponent} />;
+}
+
+export default App;
